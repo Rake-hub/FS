ac1d16dade4f7a8e28aad14ac828c83602e19392
Álvaro Alcaide <48095129+Rake-hub@users.noreply.github.com>
2022-10-21 15:10:12 +0200

    a

---
+++ Programador Fullstack/HTML/formulario/recogedatos.php
@@ -0,0 +1,10 @@
+<body>
+    <h1>Tus datos son:</h1>
+    <?php
+    echo('Email: '.$_GET['email']);
+    echo('<BR>');
+    echo('Nombre: '.$_GET['name']);
+    echo('<BR>');
+    echo('Ciudad: '.$_GET['city']);
+    ?>
+</body>
